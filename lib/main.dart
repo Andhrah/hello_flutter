ac05d9d8a949c98ce567f9dd472003c6d8bf8341
Alexandra Collins <andracolexa@gmail.com>
2020-02-05 10:28:11 +0100

    add a color design and app title  with material widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,15 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    Center(child: Text(
+    MaterialApp(
+      title: "Hello Flutter App",
+      home: Material(
+        color: Colors.deepOrange,
+        child: Center(child: Text(
       "Hello Andra from Flutter!",
       textDirection: TextDirection.ltr,
     ))
+      )
+    )
   );
 }
