aab8b314b456fdd079728f2b38ecc413521649b4
Alexandra Collins <andracolexa@gmail.com>
2020-02-05 11:46:36 +0100

    implement scaffold widgets

--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,21 @@ void main() {
   runApp(
     MaterialApp(
       title: "Hello Flutter App",
-      home: Material(
+      home: Scaffold(
+      // Scaffold widget allows us to create application bar
+      appBar: AppBar(
+        title: Text("Andra Flutter")
+      ),
+      body: Material(
         color: Colors.deepOrange,
         child: Center(child: Text(
       "Hello Andra!",
       textDirection: TextDirection.ltr,
       style: TextStyle(color: Colors.white, fontSize: 36.0
       ),
-    ))
+    )
+    )
+      ),
       )
     )
   );
